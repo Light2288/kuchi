bd0463c6bf204c83009927f6d5be4151fc5257f3
Light2288 <davide.aliti@gmail.com>
2022-10-04 01:16:48 +0200

    add user manager and button to register view

--- Shared/KuchiApp.swift
+++ Shared/KuchiApp.swift
@@ -2,9 +2,16 @@ import SwiftUI
 
 @main
 struct KuchiApp: App {
+    let userManager = UserManager()
+    
+    init() {
+        userManager.load()
+    }
+    
   var body: some Scene {
     WindowGroup {
         RegisterView()
+            .environmentObject(userManager)
     }
   }
 }
--- Shared/Welcome/RegisterView.swift
+++ Shared/Welcome/RegisterView.swift
@@ -3,7 +3,9 @@ import SwiftUI
 
 struct RegisterView: View {
     // MARK: - Properties
-    @State var name: String = ""
+    @EnvironmentObject var userManager: UserManager
+    
+    @FocusState var nameFieldFocused: Bool
     
     // MARK: - Body
     var body: some View {
@@ -11,8 +13,33 @@ struct RegisterView: View {
             Spacer()
             
             WelcomeMessageView()
-            TextField("Type your name...", text: $name)
+            TextField("Type your name...", text: $userManager.profile.name)
+                .submitLabel(.done)
                 .bordered()
+            
+            HStack {
+                Spacer()
+                
+                Text("\(userManager.profile.name.count)")
+                    .font(.caption)
+                    .foregroundColor(
+                        userManager.isUserNameValid() ? .green : .red
+                    )
+                    .padding(.trailing)
+            }
+            
+            Button(action: registerUser) {
+                HStack {
+                    Image(systemName: "checkmark")
+                        .resizable()
+                        .frame(width: 16, height: 16, alignment: .center)
+                    Text("OK")
+                        .font(.body)
+                        .bold()
+                }
+            }
+            .bordered()
+            .disabled(!userManager.isUserNameValid())
             
             Spacer()
         }
@@ -21,9 +48,19 @@ struct RegisterView: View {
     }
 }
 
+// MARK: - Event Handlers
+extension RegisterView {
+    func registerUser() {
+        userManager.persistProfile()
+    }
+}
+
 // MARK: - Preview
 struct RegisterView_Previews: PreviewProvider {
+    static let user = UserManager(name: "Ray")
+    
     static var previews: some View {
         RegisterView()
+            .environmentObject(user)
     }
 }
